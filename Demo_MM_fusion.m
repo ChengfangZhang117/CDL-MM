0600896dcd2983b5994790f4a20fe97c72c4d582
chengfang Zhang <33707855+zcf627759026@users.noreply.github.com>
2022-12-22 11:03:14 +0800

    Update Demo_MM_fusion.m

--- Demo_MM_fusion.m
+++ Demo_MM_fusion.m
@@ -17,15 +17,14 @@ D_joint=[outputCDL.Psi_cx outputCDL.Psi_x O;outputCDL.Psi_cy O outputCDL.Psi_y];
 %% Fusion
 method = {'CDL_song'};
 params.method = method;
-Evaluation_index = {'QS','Æ½¾ùÌİ¶È','±ßÔµÇ¿¶È','±ê×¼²î','»Ò¶È¾ùÖµ','Í¼ÏñÇåÎú¶È','QG','Qabf','Q0','Qw','Qe','NMI','QTE','QNCIE','QM','QSF','QP','QC','QY','QCV','QCB','psnr','rmse','¿Õ¼äÆµÂÊ','ssim','viff'};
 test_image=mygetdirfiles('test_images');
 test_imagecell=load_image(test_image);
 index_a = 1:2:size(test_imagecell);
 index_b = 2:2:size(test_imagecell);
 A=cell(size(index_a,2),1);
 B=cell(size(index_b,2),1);
-k = 1;%%ÏÔÊ¾·½·¨
-t = 1;%%¿ØÖÆ·½·¨ĞĞÊı
+k = 1;%%æ˜¾ç¤ºæ–¹æ³•
+t = 1;%%æ§åˆ¶æ–¹æ³•è¡Œæ•°
 q = 1;
 m = 1;%%sheet
 for i = 1:numel(test_imagecell)/2
@@ -34,12 +33,7 @@ for i = 1:numel(test_imagecell)/2
     params.p = p;
     params.n = n;
     params.x1 = x;
-    xlswrite('EvalResult.xls',{n},m,['A',num2str(t)]);
-    xlswrite('EvalResult.xls',Evaluation_index,m,['B',num2str(t)]);
-    xlswrite('EvalResult.xls',method',m,['A',num2str(t+1)]);
-    xlswrite('time.xls',{n},m,['A',num2str(t)]);
-    xlswrite('time.xls',method',m,['A',num2str(t+1)]);
-     if size(test_imagecell{index_a(i)},3)>1
+   if size(test_imagecell{index_a(i)},3)>1
     A{i}=double(rgb2gray(test_imagecell{index_a(i)}));
     B{i}=double(rgb2gray(test_imagecell{index_b(i)}));
     else
@@ -49,20 +43,17 @@ for i = 1:numel(test_imagecell)/2
     tic;
     y_F_CDL_song{i}=func_MM_fusion(A{i},B{i},outputCDL.Psi_x,outputCDL.Psi_cx,outputCDL.Psi_y,outputCDL.Psi_cy,D_joint);
      time_CDL_song=toc;
-    EvalResult_CDL_song = Evaluation(A{i},B{i},double(y_F_CDL_song{i}),256);
-    xlswrite('EvalResult.xls', EvalResult_CDL_song,m,['B',num2str(t+1)]);
-    xlswrite('time.xls', time_CDL_song,m,['B',num2str(t+1)]);
       
     t = t + size(method,2) + 1;
     q = q + 3;
     k = 1;
-%%%½«¸÷ÖÖ·½·¨ËùµÃµ½µÄÈÚºÏÍ¼Ïñ·Åµ½Í¬Ò»¸ö´óµÄ¾ØÕóÖĞ
+%%%å°†å„ç§æ–¹æ³•æ‰€å¾—åˆ°çš„èåˆå›¾åƒæ”¾åˆ°åŒä¸€ä¸ªå¤§çš„çŸ©é˜µä¸­
    result = cat(3,y_F_CDL_song{i});
    conf.fusion_image{i} = {};
-%%%%½«ÈÚºÏÍ¼ÏñĞ´Èëµ½Ö¸¶¨µÄÎÄ¼ş¼ĞÀï
+%%%%å°†èåˆå›¾åƒå†™å…¥åˆ°æŒ‡å®šçš„æ–‡ä»¶å¤¹é‡Œ
    for j = 1:numel(method)
         conf.fusion_image{i}{j} = fullfile(p, 'results', [n sprintf('[%d-%s]', j, method{j}) x]);
-        imwrite(uint8(result(:, :, j)), conf.fusion_image{i}{j});%%%%½«¸÷ÖÖ·½·¨µÄ½á¹û·Åµ½resultÎÄ¼ş¼ĞÖĞ
+        imwrite(uint8(result(:, :, j)), conf.fusion_image{i}{j});%%%%å°†å„ç§æ–¹æ³•çš„ç»“æœæ”¾åˆ°resultæ–‡ä»¶å¤¹ä¸­
     end
 end
 
